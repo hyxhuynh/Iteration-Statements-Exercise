1f4cd471b34d9cd44ae7215eb47433b188655e19
Hy Huynh <hyxhuynh.projects@gmail.com>
2024-03-29 04:08:15 -0700

    completed the exercise

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -4,30 +4,170 @@
     {
         //LukeWarm Section: Create methods below
         //Write a method that will print to the console all numbers 1000 through - 1000
+        public static void PrintNegToPosThousand()
+        {
+            // from -1000 to 1000 (increment)
+            //for (int i = -1000; i < 1001; i++)
+
+            Console.WriteLine("Print to the console all numbers 1000 through - 1000:");
+            // from 1000 to -1000 (decrement)
+            for (int i = 1000; i >= -1000; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("---------------------------------------");
+        }
 
         //Write a method that will print to the console numbers 3 through 999 by 3 each time
+        public static void PrintDivisibleByThree()
+        {
+            Console.WriteLine("\nPrint to the console numbers 3 through 999 by 3 each time");
+            for (int i = 3; i < 1000; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("---------------------------------------");
+        }
 
         //Write a method to accept two integers as parameterss and check whether they are equal or not
-        
+        public static bool CheckEqual(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                return true;
+            }
+            return false;
+        }
+
         //Write a method to check whether a given number is even or odd
-        
+        public static void CheckEvenOrOdd(int num)
+        {
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"{num} is an EVEN number.");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is an ODD number.");
+            }
+        }
+
         //Write a method to check whether a given number is positive or negative
-        
+        public static void CheckPosOrNeg(int num)
+        {
+            if (num > 0)
+            {
+                Console.WriteLine($"{num} is a POSITIVE number.");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine($"{num} is a NEGATIVE number.");
+            }
+            else
+            {
+                Console.WriteLine("Zero is neither positive or negative.");
+            }
+        }
+
         //Write a method to read the age of a candidate and determine whether they can vote.
         //Hint: Use Parse or the safer TryParse() for an extra challenge
         //Parse()
         //TryParse()
 
+        public static void CanVote(int age)
+        {
+            if (age >= 18) 
+            {
+                Console.WriteLine("You can vote.");
+            }
+            else
+            {
+                Console.WriteLine($"{age} years old is too young to vote. You have to be at least 18.");
+            }
+        }
+
         //Heatin Up Section:
         //Write a method to check if an integer(from the user) is in the range -10 to 10
-        
+        public static bool CheckRange(int num)
+        {
+            if (num >= -10 && num <= 10)
+                return true;
+            return false;
+        }
+
         //Write a method to display the multiplication table(from 1 to 12) of a given integer
+        public static void MultiplicationTable(int x)
+        {
+            int[] multiTable = new int[12];
+            Console.WriteLine($"\nMultiplication table (from 1 to 12) of {x}");
+            for (int i = 1; i <= 12; i++)
+            {
+                multiTable[i - 1] = i * x;
+                Console.WriteLine($"{i} * {x} = {multiTable[i - 1]}");
+            }
+        }
 
 
         //Call the methods to test them in the Main method below
         static void Main(string[] args)
         {
-            
+;           PrintNegToPosThousand();
+            //----------------------------------------------------
+
+            PrintDivisibleByThree ();
+            //----------------------------------------------------
+
+            Console.WriteLine("\nEnter the first integer number:");
+            int userNum1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second integer number:");
+            int userNum2 = int.Parse(Console.ReadLine());
+
+            bool isEqual = CheckEqual(userNum1, userNum2);
+            Console.WriteLine("If equal, print \"True\"; if NOT, print \"False\":");
+            Console.WriteLine(isEqual);
+            //----------------------------------------------------
+
+            Console.WriteLine("\nEnter an integer number:");
+            int userNumber = int.Parse(Console.ReadLine());
+
+            CheckEvenOrOdd(userNumber);
+            //----------------------------------------------------
+
+            Console.WriteLine("\nEnter an integer number:");
+            int userNum = int.Parse(Console.ReadLine());
+
+            CheckPosOrNeg(userNum);
+
+            //----------------------------------------------------
+            //If the string can not be converted to an integer, then
+            //int.Parse() will throw an exception
+            //int.TryParse() will return false (but not throw an exception)
+
+            bool isParsed;
+            int parsedAge;
+            Console.WriteLine("\nEnter your age:");
+            string inputAge = Console.ReadLine();
+            isParsed = int.TryParse(inputAge, out parsedAge);
+
+            Console.WriteLine($"Boolean value to determine if \"{inputAge}\" is parsed as an integer: {isParsed}");
+            if (isParsed) { CanVote(parsedAge); }
+
+            //----------------------------------------------------
+            Console.WriteLine("\nEnter an integer number:");
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"If {inputNumber} is in the range of -10 and 10, print \"True\"; if NOT, print \"False\":");
+            bool isInRange = CheckRange(inputNumber);
+            Console.WriteLine(isInRange);
+
+            //----------------------------------------------------
+            Console.WriteLine("\nEnter an integer number:");
+            int inputNum = int.Parse(Console.ReadLine());
+
+            MultiplicationTable(inputNum);
+
         }
     }
 }
